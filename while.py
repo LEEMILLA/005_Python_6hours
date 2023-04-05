9ccecaf3f89414b760722be5aab848351e8c98d1
LEEMILLA <leemilla605@gmail.com>
2023-04-05 21:47:44 +0900

    5days

--- while.py
+++ while.py
@@ -2,7 +2,15 @@
 customer = "토르"
 index = 5
 while index >= 1:
-    print("{0} 손님 ㅓㅋ피가 준비되었습니다. {1}번 남았어요".format(customer, index))
+    print("{0} 손님 커피가 준비되었습니다. {1}번 남았어요".format(customer, index))
     index -= 1
     if index == 0:
         print("커피는 폐기처분 되었습니다.")
+
+customer = "아이언맨"
+index = 1
+while True:
+    print("{0}, 커피가 준비 되었습니다. {1}회 호출.".format(customer, index))
+    index += 1
+    if index > 10000:
+        break
