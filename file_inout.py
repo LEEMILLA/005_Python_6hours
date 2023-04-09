e6ec5333d8e464c8da07ace61ec0ae1904914d1b
LEEMILLA <leemilla605@gmail.com>
2023-04-09 22:38:42 +0900

    7days

--- file_inout.py
+++ file_inout.py
@@ -3,7 +3,34 @@
 # print("영어 : 50", file=score_file)
 # score_file.close()
 
-# score_file = open("score.txt", "a", encoding="utf-8")
-# score_file.write("과학: 80")
-# score_file.write("\n코딩: 100")
+# score_file = open("score.txt", "a", encoding="utf8")
+# score_file.write("과학 : 80")
+# score_file.write("\n코딩 : 100")
 # score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf-8")
+# print(score_file.read())
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.readline(), end="") #줄별로 읽기, 한 줄 읽고 커서는 다음 줄로 이동
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# print(score_file.readline())
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# while True:
+#     line = score_file.readline()
+#     if not line:
+#         break
+#     print(line, end="")
+# score_file.close()
+
+
+score_file = open("score.txt", "r", encoding="utf8")
+lines = score_file.readlines(0)
+for line in lines:
+    print(line, end="")
+
+score_file.close()
