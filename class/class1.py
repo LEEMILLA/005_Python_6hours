98b46262a879daea0e3460d5ab80df54f168b4ab
LEEMILLA <leemilla605@gmail.com>
2023-04-11 21:57:58 +0900

    9days

---
+++ class/class1.py
@@ -0,0 +1,44 @@
+# # 마린: 공격 유닛, 군인. 총을 쏨
+# name = "마린"
+# hp = 40
+# damage = 5
+
+# print("{0} 유닛이 생성되었습니다.".format(name))
+# print("체력 {0}, 공격력{1} \n".format(hp, damage))
+
+# tank_name = "탱크"
+# tank_hp = 150
+# tank_damage = 35
+
+# print("{0} 유닛이 생성되었습니다.".format(tank_name))
+# print("체력 {0}, 공격력{1} \n".format(tank_hp, tank_damage))
+
+# def attack(name, location, damage):
+#     print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]".format(name, location, damage))
+
+# attack(name, "1시", damage)
+# attack(tank_name, "1시", tank_damage)
+
+
+class Unit:
+    def __init__(self, name, hp, damage):  # __init__: 파이썬에서 쓰이는 생성자
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        print("{0} 유닛이 생성 되었습니다.".format(self.name))
+        print("체력 {0}, 공격력{1}".format(self.hp, self.damage))
+
+
+# marine1 = Unit("마린", 40, 5)
+# marine2 = Unit("마린", 40, 5)
+# tank = Unit("탱크", 150, 35)
+
+
+wraith1 = Unit("레이스", 80, 5)
+print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))
+
+# 마인드 컨트롤
+wraith2 = Unit("빼앗은 레이스", 80, 5)
+wraith2.clocking = True
+if wraith2.clocking == True:
+    print("{0} 는 현재 클로킹 상태입니다.".format(wraith2.name))
